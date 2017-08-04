803d17b57334db5b0cadbc3f3874031fdd4d01ba
huangxiulan <huangxiulan_cd@keruyun.com>
2017-08-04 18:29:29 +0800

    修改电话号码

--- src/js/result.js
+++ src/js/result.js
@@ -1,6 +1,6 @@
 var Dialog = require('../components/dialog/dialog');
 var dialog = new Dialog(2000);
 
-$(document).on('click', '[data-btn="apply"]', function () {
+$('body').on('click', '[data-btn="apply"]', function () {
     dialog.tips('报名成功，稍后会有工作人员跟您联系。');
 });
