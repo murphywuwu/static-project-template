dcc96bb65e5b73942a7ec3fcacd3daac3f9ce9ba
huangxiulan <huangxiulan_cd@keruyun.com>
2017-08-07 13:29:09 +0800

    样式调整

--- src/js/result.js
+++ src/js/result.js
@@ -1,6 +1,12 @@
 var Dialog = require('../components/dialog/dialog');
 var dialog = new Dialog(2000);
 
-$('[data-btn="apply"]').on('click', function () {
-    dialog.tips('报名成功，稍后会有工作人员跟您联系。');
+$('[data-btn="apply"]').on('click', function ( e ) {
+    var $target = $(e.target);
+    var is_apply = $target.hasClass('success');
+
+    if ( !is_apply ) {
+        $target.addClass('success');
+        dialog.tips('报名成功，稍后会有工作人员跟您联系。');
+    };
 });
